71895ab7e93ff51e6d6ad39cfdd251f57949a42a
Kiruncio <mistermobidic@gmail.com>
2025-10-06 12:39:09 -0500

    Clase 1 - semana 2

--- src/E2_Empleado.java
+++ src/E2_Empleado.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class E2_Empleado {
     String nombre;
     String cargo;
@@ -12,7 +14,46 @@ public class E2_Empleado {
     }
 
     void mostrar_info(){
+        System.out.println("Empleado: "+nombre+"   Cargo: "+cargo+
+                "\nSalario: "+salario+"   Fecha de ingreso: "+ingreso+"\n");
+    }
 
+    public static void main(String[]args){
+        Scanner detalles = new Scanner(System.in);
+        System.out.println("Ingrese el nombre del empleado 1: ");
+        String nombre1 = detalles.nextLine();
+        System.out.println("Ingrese su cargo: ");
+        String cargo1 = detalles.nextLine();
+        System.out.println("Ingrese su salario: ");
+        double salario1 = detalles.nextDouble();
+        detalles.nextLine();
+        System.out.println("Ingrese su fecha de ingreso (dd/mm/aa): ");
+        String fechain1 = detalles.nextLine();
+        E2_Empleado empleado1 = new E2_Empleado(nombre1,cargo1,salario1,fechain1);
+        System.out.println("Ingrese el nombre del empleado 2: ");
+        String nombre2 = detalles.nextLine();
+        System.out.println("Ingrese su cargo: ");
+        String cargo2 = detalles.nextLine();
+        System.out.println("Ingrese su salario: ");
+        double salario2 = detalles.nextDouble();
+        detalles.nextLine();
+        System.out.println("Ingrese su fecha de ingreso (dd/mm/aa): ");
+        String fechain2 = detalles.nextLine();
+        E2_Empleado empleado2 = new E2_Empleado(nombre2,cargo2,salario2,fechain2);
+        System.out.println("Ingrese el nombre del empleado 3: ");
+        String nombre3 = detalles.nextLine();
+        System.out.println("Ingrese su cargo: ");
+        String cargo3 = detalles.nextLine();
+        System.out.println("Ingrese su salario: ");
+        double salario3 = detalles.nextDouble();
+        detalles.nextLine();
+        System.out.println("Ingrese su fecha de ingreso (dd/mm/aa): ");
+        String fechain3 = detalles.nextLine();
+        E2_Empleado empleado3 = new E2_Empleado(nombre3,cargo3,salario3,fechain3);
+
+        empleado1.mostrar_info();
+        empleado2.mostrar_info();
+        empleado2.mostrar_info();
     }
 
 }
